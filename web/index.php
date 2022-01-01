cac9eb9302e2622aa2ae0fb0972bfd35fce35fe9
Pierre-Henry Soria <hi@ph7.me>
2022-01-02 09:29:25 +1030

    Rename config.php to constants.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
-require __DIR__ . '/config.php';
+require __DIR__ . '/constants.php';
 
 use PierreHenry\PodcastGenerator\Podcast\Feed\Generator;
 
