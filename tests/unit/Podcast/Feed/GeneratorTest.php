b4fd12a8b2e9df021133a302c9e7e328a5e261ff
Pierre-Henry Soria <hi@ph7.me>
2022-01-01 18:42:10 +1030

    Add more tests

--- tests/unit/Podcast/Feed/GeneratorTest.php
+++ tests/unit/Podcast/Feed/GeneratorTest.php
@@ -15,6 +15,13 @@ class GeneratorTest extends TestCase
 {
     private Generator $generator;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->generator = new Generator(UNIT_TEST_ROOT_PATH . 'dataset/files', 'http://localhost:2021');
+    }
+
     public function testOutputFeed(): void
     {
         $actual = $this->generator->outputFeed(false);
@@ -25,12 +32,5 @@ class GeneratorTest extends TestCase
         $this->assertMatchesRegularExpression('/<description>/', $actual);
         $this->assertMatchesRegularExpression('/' . PODCAST_NAME . '/', $actual);
         $this->assertMatchesRegularExpression('/' . PODCAST_DESCRIPTION . '/', $actual);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->generator = new Generator(UNIT_TEST_ROOT_PATH . 'dataset/files', 'http://localhost:2021');
     }
 }
---
+++ tests/unit/Podcast/Xml/FeedTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) Pierre-Henry Soria <hi@pH7.me>
+ * MIT License - https://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace PierreHenry\PodcastGenerator\Unit\Test\Podcast\Rss;
+
+use PHPUnit\Framework\TestCase;
+use PierreHenry\PodcastGenerator\Xml\Rss\Feed;
+
+class FeedTest extends TestCase
+{
+    private Feed $feed;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->feed = new Feed('Dummy Title', 'http://localhost', 'Dummy Description');
+    }
+
+    public function testFeed(): void
+    {
+        $data = [
+            [
+                'title' => 'Title',
+                'path' => UNIT_TEST_ROOT_PATH . 'dataset/files/audio.mp3',
+                'url' => 'http://localhost',
+                'description' => 'Description'
+            ]
+        ];
+
+        $this->feed->generate($data);
+        $actual = $this->feed->saveXML();
+
+        $this->assertIsString($actual);
+        $this->assertMatchesRegularExpression('/<title>/', $actual);
+        $this->assertMatchesRegularExpression('/<description>/', $actual);
+        $this->assertMatchesRegularExpression('/Title/', $actual);
+        $this->assertMatchesRegularExpression('/Description/', $actual);
+    }
+}
